1d06de7660bde1aa23d7d3d7cfed94f5af528f56
alexander grajales <alex-12-04@hotmail.com>
2017-01-03 16:54:40 -0500

    se organizan archivos incompletos con campo fecha

--- app/src/main/java/com/obando/mejia/arrendapp/DateActivity.java
+++ app/src/main/java/com/obando/mejia/arrendapp/DateActivity.java
@@ -1,12 +1,14 @@
 package com.obando.mejia.arrendapp;
 
 import android.app.DatePickerDialog;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class DateActivity extends AppCompatActivity {
 
@@ -23,6 +25,24 @@ public class DateActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void onStart()
+    {
+        super.onStart();
+        EditText txtDate= (EditText)findViewById(R.id.txtdate);
+        txtDate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if(hasFocus)
+                {
+                    DateDialog dialog = new DateDialog(view);
+                    FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                    dialog.show(fragmentTransaction, "DatePicker");
+                }
+
             }
         });
     }
--- app/src/main/java/com/obando/mejia/arrendapp/DateDialog.java
+++ app/src/main/java/com/obando/mejia/arrendapp/DateDialog.java
@@ -5,7 +5,10 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
+
+import java.util.Calendar;
 
 /**
  * Created by ALEXANDER on 03/01/2017.
@@ -14,11 +17,23 @@ import android.widget.EditText;
 public class DateDialog extends DialogFragment implements DatePickerDialog.OnDateSetListener {
 
     EditText txtDate;
-
     public DateDialog(View view)
     {
-        txtDate = (EditText) view;
+        txtDate = (EditText)view;
     }
-    public Dialog onCreateDialog(Bundle saveInstance)
 
+    public Dialog onCreateDialog(Bundle saveInstanceState)
+    {
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    public void onDateSet(DatePicker view, int year, int month, int day)
+    {
+        String date = day+"-"+(month+1)+"-"+year;
+        txtDate.setText(date);
+    }
 }

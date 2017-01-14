92ed7bdc180acc860547b35cb3f732648f492913
Cristian Mejia <alveiro09@hotmail.com>
2017-01-14 11:32:40 -0500

    Se agregan iconos se crea menu de seleccion sobre cada item de la lista  de usuarios

--- app/src/main/java/com/obando/mejia/arrendapp/Controlador/Adapter/UsuariosAdapter.java
+++ app/src/main/java/com/obando/mejia/arrendapp/Controlador/Adapter/UsuariosAdapter.java
@@ -1,43 +1,51 @@
 package com.obando.mejia.arrendapp.Controlador.Adapter;
 
+//region Librerias
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.obando.mejia.arrendapp.Modelo.Entidades.ClsUsuario;
 import com.obando.mejia.arrendapp.R;
-
 import java.util.ArrayList;
+import static com.obando.mejia.arrendapp.Controlador.Activity.MainActivity.ListaUsuarios;
+//endregion Librerias
 
 /**
  * Created by personal on 13/01/2017.
  */
 
 public class UsuariosAdapter extends RecyclerView.Adapter<UsuariosAdapter.MyvistaHolder> {
+    //region Propiedades Adaptador
     private Context fContexto;
     private ArrayList<ClsUsuario> fUsuarios;
+    //endregion Propiedades Adaptador
 
+    //region Constructor Adaptador
     public UsuariosAdapter(Context paramContext, ArrayList<ClsUsuario> paramUsuarios) {
         this.fContexto = paramContext;
         this.fUsuarios = paramUsuarios;
     }
+    //endregion Constructor Adaptador
 
+    //region ViewHolder -- Cada Item
+
+    //region Constructor
     @Override
     public MyvistaHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         return new MyvistaHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.lista_usuarios, null));
-
-
     }
-
+    //endregion Constructor
+    //region Metodos Sobrecargados
     @Override
     public void onBindViewHolder(MyvistaHolder holder, final int position) {
-        holder.tNombre.setText(fUsuarios.get(position).getNombre());
-        holder.tApellido.setText(fUsuarios.get(position).getApellido());
-        holder.tNombreUsuario.setText(fUsuarios.get(position).getNombreUsuario());
+        holder.fTxtNombre.setText(fUsuarios.get(position).getNombre());
+        holder.fTxtApellido.setText(fUsuarios.get(position).getApellido());
+        holder.fTxtNombreUsuario.setText(fUsuarios.get(position).getNombreUsuario());
     }
 
 
@@ -45,31 +53,56 @@ public class UsuariosAdapter extends RecyclerView.Adapter<UsuariosAdapter.Myvist
     public int getItemCount() {
         return fUsuarios.size();
     }
+    //endregion Metodos Sobrecargados
 
-
-    public class MyvistaHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener{
-        TextView tNombre, tApellido, tNombreUsuario;
+    public class MyvistaHolder extends RecyclerView.ViewHolder implements
+            View.OnClickListener, View.OnLongClickListener {
+        TextView fTxtNombre, fTxtApellido, fTxtNombreUsuario;
+        Integer fPosicionItem;
 
         public MyvistaHolder(View itemView) {
             super(itemView);
 
-            tNombre = (TextView) itemView.findViewById(R.id.Nombre);
-            tApellido = (TextView) itemView.findViewById(R.id.Apellido);
-            tNombreUsuario = (TextView) itemView.findViewById(R.id.NombreUsuario);
+            fTxtNombre = (TextView) itemView.findViewById(R.id.Nombre);
+            fTxtApellido = (TextView) itemView.findViewById(R.id.Apellido);
+            fTxtNombreUsuario = (TextView) itemView.findViewById(R.id.NombreUsuario);
+            fPosicionItem =  getAdapterPosition();
+
+            itemView.setOnClickListener(this);
+            itemView.setOnLongClickListener(this);
+
+            itemView.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener() {
+
+                @Override
+                public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+                    menu.setHeaderTitle("Seleccione ");
+                    menu.setHeaderIcon(R.drawable.ic_mode_edit_black_48dp);
+                    menu.add("Editar").setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem item) {
+                            return true;
+                        }
+                    });
+                    menu.add("Eliminar").setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem item) {
+                            ListaUsuarios.remove(fPosicionItem);
+                            return true;
+                        }
+                    });
+                }
+            });
         }
+
 
         @Override
         public void onClick(View view) {
-            Toast.makeText(fContexto,"Click",Toast.LENGTH_SHORT).show();
         }
 
         @Override
         public boolean onLongClick(View view) {
-
-            Toast.makeText(fContexto,"Long Click",Toast.LENGTH_LONG).show();
             return false;
         }
     }
-
-
+    //endregion ViewHolder -- Cada Item
 }
